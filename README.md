2438c37aadcc455d53bc0605fc32fefd49bd08ad
cksk-jp <carlson.ksk@gmail.com>
2023-11-29 09:52:28 -0500

    styling updates, readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fruit-search
 Project: To create a search bar where the user is able to query different fruits. Results should populate as the user types their query.
+
+# Changelog
+- Updated commenting for functions
+- Hot-fixed user drag bug
+- UI improvements and responsiveness to browser
+- CSS clean-up of unused properties
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-	
+	// main fruit filter logic
 	function search(str) {
 		let results = [];
 		str = str.toLowerCase();
@@ -19,6 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		return results;
 	}
 
+	// main user input check, 
 	const searchHandler = (event) => {
 		text = event.target.value;
 		if (text === '') {
@@ -34,13 +35,19 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
+	// render suggestions under input bar
 	function showSuggestions(results, inputVal) {
 		console.log('suggestions generating');
 		inputText = inputVal.toLowerCase();
 		suggestions.innerHTML = '';
+		// if no results, then remove class
+		if (results.length === 0) {
+			suggestions.classList.remove('has-suggestions');
+		}
 		for (let fruit of results) {
 			let checkedFruit = fruit.toLowerCase();
 			let fruitNode = document.createElement('li');
+			// use slice to insert span for bolding of search input
 			let spanStartIndex = checkedFruit.indexOf(inputText);
 			let spanEndIndex = spanStartIndex + inputText.length;
 			
@@ -55,7 +62,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	//TODO: bug when dragging 
+	// check for user selected suggestion
 	function useSuggestion(e) {
 		selectedFruit = e.target.closest('li');
 		input.value = selectedFruit.innerText;
